34d53bb2372dfd9d840c7dc684317ff779ee86da
DanyMazze <daniele.mazzetti08@gmail.com>
2026-05-05 09:15:44 +0200

    feat: aggiunta la funzionalità e il controllo dell'input utente e l'aumento del punteggio backend, inizio fase 8 con lo stato del gioco 'PlAYING' o 'RESULT' nel file main.py. creazione del config nel file config.py.

--- brain_shift/ui.py
+++ brain_shift/ui.py
@@ -1,22 +1,23 @@
 import pygame
 from models import Trial
+import config
 
 def draw_card(surface, trial: Trial, config):
-    card_width = config["card_width"]
-    card_height = config["card_height"]
-    x = (config["screen_width"] - card_width) // 2
+    card_width = config.CARD_WIDTH
+    card_height = config.CARD_HEIGHT
+    x = (config.SCREEN_WIDTH - card_width) // 2
 
     if trial.position == "TOP":
-        y = config["top_y"]
+        y = config.TOP_Y
     else:
-        y = config["bottom_y"]
+        y = config.BOTTOM_Y
 
     card_rect = pygame.Rect(x, y, card_width, card_height)
-    pygame.draw.rect(surface, config['card_color'], card_rect)
+    pygame.draw.rect(surface, config.CARD_COLOR, card_rect)
 
-    font = pygame.font.Font(config['font_name'], config['font_size'])
+    font = pygame.font.Font(config.FONT_NAME, config.FONT_SIZE)
     text = f"{trial.letter}{trial.number}"
-    text_surface = font.render(text, True, config['text_color'])
+    text_surface = font.render(text, True, config.TEXT_COLOR)
     text_rect = text_surface.get_rect(center=card_rect.center)
 
     surface.blit(text_surface, text_rect)
